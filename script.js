bc376c07436a1136994c6141c3aca74ecec1f641
Hanz Felix Tumulak <tumulakhanz@gmail.com>
2022-12-16 16:16:48 +0800

    Add function to add cards automatically

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 function addCards() {
     for (i=0;i<5;i++) {  
-     var v = document.createElement('input');
-     v.type="button";
-     v.value="Button " +i;
-     document.getElementById('test').appendChild(v);
+     var v = document.createElement('div');
+     v.classList.add("card");
+     
+     var a = document.createElement('a');
+
+     var cardBg = document.createElement('div');
+     cardBg.classList.add("card-bg");
+
+     var btnEdit = document.createElement('button');
+     btnEdit.classList.add("edit-button");
+     btnEdit.classList.add("hidden");
+     btnEdit.textContent = "edit"
+
+     var p = document.createElement('p');
+     p.textContent = "Hello"
+     
+     cardBg.appendChild(btnEdit)
+     cardBg.appendChild(p)
+     a.appendChild(cardBg)
+     v.appendChild(a)
+
+     document.getElementById('container').appendChild(v);
+     console.log("clicked!")
     }
 }
