bd1138747b2017931d802a8feda7f9e6fc31b677
Hanz Felix Tumulak <tumulakhanz@gmail.com>
2022-12-18 12:36:28 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
+var add = document.getElementById("add-btn");
+const gridComputedStyle = getComputedStyle(document.getElementById("grid"));
+var cardCount = 2;
+add.style.order = cardCount+1;
+
 function addCard() {
+     var gridRowCount = gridComputedStyle.getPropertyValue("grid-template-rows").split(" ").length;
+     var gridColCount = gridComputedStyle.getPropertyValue("grid-template-columns").split(" ").length;
+
      var v = document.createElement('div');
      v.classList.add("card");
      v.classList.add("new-card");
@@ -7,20 +15,26 @@ function addCard() {
      a.href = "#";
 
      var cardBg = document.createElement('div');
-     cardBg.classList.add("card-bg");
+     cardBg.classList.add("card-url");
 
      var btnEdit = document.createElement('button');
      btnEdit.classList.add("edit-button");
      btnEdit.classList.add("hidden");
-     btnEdit.textContent = "edit"
+     btnEdit.textContent = "•••"
 
      var p = document.createElement('p');
-     p.textContent = "Hello"
-     
+     p.textContent = "Very long title that exceeds a few lines"
+
      cardBg.appendChild(btnEdit)
      cardBg.appendChild(p)
      a.appendChild(cardBg)
      v.appendChild(a)
 
-     document.getElementById('container').appendChild(v);
+     document.getElementById('grid').appendChild(v);
+     cardCount++;
+
+     add.style.order = cardCount+1
+     //add.classList.remove("add");
+     //add.classList.add("add");
+     // temp
 }
