e2e315f6107c8a4c94cd23658d4128908b1507a9
Hanz Felix Tumulak <tumulakhanz@gmail.com>
2023-03-16 10:00:18 +0800

    Create temporary deploy script

---
+++ deploy.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env sh
+
+# abort on errors
+#set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git checkout gh-pages
+git add -A
+git commit -m 'Deploy page'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push origin gh-pages
+
+cd -
+
+read  -n 1 -p "Input Selection:" mainmenuinput
+
+
+# help, need stable way to auto push to gh-pages
