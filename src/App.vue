c7567a465f47714dbd94d7c05a2446b4d48d17ea
Hanz Felix Tumulak <tumulakhanz@gmail.com>
2023-06-26 07:03:32 +0800

    Fix edit mode button in light mode

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import AddModal from "./components/AddModal.vue";
 import EditModal from "./components/EditModal.vue";
 import PopUpModal from "./components/PopUpModal.vue";
 import { useCounterStore } from "./stores/counter";
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
 
 const counterStore = useCounterStore();
 const reader = new FileReader();
@@ -42,12 +42,12 @@ function loadJsonFile(e) {
         v-model="counterStore.prevent_edit"
       />
       <div
-        class="material-icons peer h-6 w-12 rounded-full bg-slate-400 transition-colors after:absolute after:-top-1 after:-left-2 after:h-8 after:w-8 after:rounded-full after:border-2 after:border-transparent after:bg-slate-200 after:pt-0.5 after:text-center after:text-slate-600 after:transition-all after:content-['drive\_file\_rename\_outline'] peer-checked:bg-slate-800 peer-checked:after:translate-x-full peer-checked:after:bg-slate-600 peer-checked:after:text-slate-300 peer-checked:after:content-['lock'] peer-checked:hover:bg-slate-600 peer-checked:after:hover:bg-slate-200 peer-checked:hover:after:text-slate-600 peer-focus:after:bg-slate-200 peer-focus:after:text-slate-600"
+        class="material-icons peer h-6 w-12 rounded-full bg-slate-400 bg-opacity-100 transition-colors after:absolute after:-top-1 after:-left-2 after:h-8 after:w-8 after:rounded-full after:border-2 after:border-transparent after:bg-slate-600 after:pt-0.5 after:text-center after:text-slate-200 after:transition-all after:content-['drive\_file\_rename\_outline'] peer-checked:bg-opacity-0 peer-checked:after:translate-x-full peer-checked:after:bg-slate-400 peer-checked:after:text-slate-600 peer-checked:after:content-['lock'] peer-checked:hover:bg-slate-400 peer-checked:after:hover:bg-slate-600 peer-checked:hover:after:text-slate-200 peer-focus:after:bg-slate-600 peer-focus:after:text-slate-200 dark:bg-slate-600 dark:after:bg-slate-200 dark:after:text-slate-600 dark:peer-checked:after:bg-slate-600 dark:peer-checked:after:text-slate-200 dark:peer-checked:hover:bg-slate-600 dark:peer-checked:after:hover:bg-slate-200 dark:peer-checked:hover:after:text-slate-600 dark:peer-focus:after:bg-slate-200 dark:peer-focus:after:text-slate-600"
       ></div>
     </label>
     <span
-      class="peer/a ml-3 text-sm font-medium peer-hover/a:text-slate-300"
-      :class="counterStore.prevent_edit ? 'text-slate-800' : 'text-slate-300'"
+      class="peer/a ml-3 text-sm font-semibold text-slate-800 transition-opacity peer-hover/a:opacity-100 dark:text-slate-300"
+      :class="counterStore.prevent_edit ? 'opacity-0' : 'opacity-100'"
       >Edit Mode</span
     >
   </div>
@@ -115,7 +115,7 @@ function loadJsonFile(e) {
           version="1.1"
           width="24"
           data-view-component="true"
-          class="octicon octicon-mark-github v-align-middle"
+          class="octicon octicon-mark-github"
         >
           <path
             d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"
